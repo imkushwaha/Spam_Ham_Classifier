92adb38acf731d23d88ae040e5fc3540e70a77d1
Upendra Kumar <upendra.kumar48762@gmail.com>
2021-11-20 11:52:43 +0530

    notebook added

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def predict():
         return render_template('index.html', prediction_text=prediction)
     
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8080")
