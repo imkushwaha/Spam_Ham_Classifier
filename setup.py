7948a099d3774861ef193534ac8e2a7d98bf40b1
Upendra Kumar <upendra.kumar48762@gmail.com>
2021-11-25 12:02:22 +0530

    circleci.config file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spam-ham-detection",
+    version="0.0.1",
+    description="ML project",
+    author="Upendra Kumar", 
+    packages=find_packages(),
+    license="MIT"
+)
